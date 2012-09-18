bf818b62d5ad2c113af8600a44b96fe811b09ef8
cgeek <cgeek.share@gmail.com>
2012-09-18 23:16:47 +0800

    router.js

--- WebRoot/assets/js/router.js
+++ WebRoot/assets/js/router.js
@@ -58,6 +58,22 @@
 
 define(function(require, exports){
 	exports.load = function(filename){
+		if (!Array.prototype.forEach)
+		{
+			Array.prototype.forEach = function(fun /*, thisp*/)
+			{
+				var len = this.length;
+				if (typeof fun != "function")
+					throw new TypeError();
+
+				var thisp = arguments[1];
+				for (var i = 0; i < len; i++)
+				{
+					if (i in this)
+						fun.call(thisp, this[i], i, this);
+				}
+			};
+		}
 		filename.split(',').forEach(function(modName){
 			require.async('./' + modName, function(mod){
 				if(mod && mod.init){
