317add7a0c39d19954576aa85def9b3e0499609a
cgeek <cgeek.share@gmail.com>
2012-10-22 01:33:32 +0800

    admin

--- WebRoot/protected/views/layouts/main.php
+++ WebRoot/protected/views/layouts/main.php
@@ -4,7 +4,7 @@
 	<title><?=$this->site_info['title'];?>-招标|招标网|招标信息|招标公告|投标|招标采购|中国权威招标采购信息门户网站</title>
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="language" content="zh" />
-	<meta name="description" content="联合招标网致力于为企业提供招标、采购、拟在建项目信息及网上招标采购等一系列商务服务。联合招标网是中国招标采购领域的权威资讯和交易网站，为各级政府采购、招标代理机构、招标企业、供应商提供强大的专业招标采购信息查询和相关服务。" />
+	<meta name="description" content="<?=$this->site_info['name'];?>致力于为企业提供招标、采购、拟在建项目信息及网上招标采购等一系列商务服务。<?=$this->site_info['name'];?>是中国招标采购领域的权威资讯和交易网站，为各级政府采购、招标代理机构、招标企业、供应商提供强大的专业招标采购信息查询和相关服务。" />
 	<meta name="keywords" content="招标,招标网,招标信息,招标采购,工程招标,政府采购,拟在建项目,招标文件,招标公告,求购信息,行业动态,商机,标讯,销售,标书,电子商务,产品,企业,服务,供应,求购,二手,投标" />
 
 	<link rel="stylesheet" type="text/css" href="<?php echo Yii::app()->request->baseUrl; ?>/css/global.css" />
@@ -18,11 +18,11 @@
 	<DIV class="g-top">
 		<UL class="g-cf g-w-990 g-mlr-a">
 			<LI class="right"><a onClick="this.style.behavior='url(#default#homepage)';this.setHomePage('http://www.unionbidding.com');" href="#"><font  color="#666666">设为首页</font></a></LI>
-			<LI class="right"><a href="#" onClick="javascript:window.external.AddFavorite('http://www.unionbidding.com','联合招标网')" title="联合招标网"><font  color="#666666">收藏本站</font></a></LI>
+			<LI class="right"><a href="#" onClick="javascript:window.external.AddFavorite('http://www.unionbidding.com','<?=$this->site_info['name'];?>')" title="<?=$this->site_info['name'];?>"><font  color="#666666">收藏本站</font></a></LI>
 			<LI class="right en g-p-r"><A href="#">广告服务</A></LI>
 			<LI class="right en g-p-r"><A href="#">邮件订阅</A></LI>
 			<LI class="right g-bi-n"><A href="#">企业入网</A> </LI>
-			<LI class="left g-c-66 en">联合招标网-中国权威招标与采购信息服务商 </LI>
+			<LI class="left g-c-66 en"><?=$this->site_info['name'];?>-中国权威招标与采购信息服务商 </LI>
 			<LI class="left g-drop"><A class="menu g-c-66" href="javascript:;"><b>中网高科旗下网站</b></A>
 				<DIV class="list sub g-d-n">
 					<P><A title="中国石化采购网" href="http://#" target="_blank">中国石化采购网</A></P>
@@ -45,7 +45,7 @@
 	<DIV class="g-mh-20">
 		<DIV id="head" class="g-cf g-mb-10  g-p-r">
 			<div style="width:220px;float:left;">
-				<A class="g-f-l" title="联合招标网(www.unionbidding.com)" href="/">
+				<A class="g-f-l" title="<?=$this->site_info['name'];?>(www.unionbidding.com)" href="/">
 				<IMG width=200 id="topLogo" src="<?=$this->site_info['logo']; ?>">
 				</A>
 			</div>
@@ -146,9 +146,9 @@
 				</tr>
 			</table>
 		</div>
-		<div style="line-height:22px;margin-top:10px;">Copyright &copy; 2009-2012 (www.unionbidding.com) Inc All Rights Reserved. [中网高科·中国联合招标网] 版权所有 <br/>
+		<div style="line-height:22px;margin-top:10px;">Copyright &copy; 2009-2012 (www.unionbidding.com) Inc All Rights Reserved. [中网高科·中国<?=$this->site_info['name'];?>] 版权所有 <br/>
 				禁止任何单位和个人以任何方式复制或建立镜像 建议使用IE浏览器 1024×768模式浏览本网站<br/>
-			 关键词：联合招标网 | 中国联合招标网 | 中网高科招标网 | 中国联合招标采购网<br/>
+			 关键词：<?=$this->site_info['name'];?> | 中国<?=$this->site_info['name'];?> | 中网高科招标网 | 中国联合招标采购网<br/>
 						 总机：4006-333-959  电话010-52883369  010-52889099 传真010-80537538<br/>
 									 京ICP备09106887号<br/>
 		</div>
